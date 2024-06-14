0515de0152ca4e75077b084e416f2d424be10128
emigpa <emigpa@gmail.com>
2024-06-14 15:02:40 -0300

    initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- change trouble config
+  {
+    "rebelot/kanagawa.nvim",
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "kanagawa",
+      },
+    },
+  },
+}
---
+++ lua/plugins/neovide.lua
@@ -0,0 +1,8 @@
+if vim.g.neovide then
+  -- vim.o.guifont = "Hack Nerd Font Mono"
+  -- vim.opt.linespace = 1
+  vim.g.neovide_scale_factor = 0.8
+  -- vim.keymap.set('n', '<C-s>', ':w<CR>') -- Save
+end
+
+return {}
