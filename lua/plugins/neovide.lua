879249da5e793a15675ba67901b3aa9981e5040f
emigpa <emigpa@gmail.com>
2024-06-18 12:06:17 -0300

    disabled volar formatting

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -1,7 +1,7 @@
 if vim.g.neovide then
   -- vim.o.guifont = "Hack Nerd Font Mono"
   -- vim.opt.linespace = 1
-  vim.g.neovide_scale_factor = 0.8
+  vim.g.neovide_scale_factor = 0.75
   -- vim.keymap.set('n', '<C-s>', ':w<CR>') -- Save
 end
 
--- lua/plugins/options.lua
+++ lua/plugins/options.lua
@@ -1,3 +1,16 @@
 return {
-  --  { "folke/persistence.nvim", enabled = false },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        volar = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "volar" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
 }
