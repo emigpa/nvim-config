cc137f04b47126d5a2c059659d4c50768fdb407f
emigpa <emigpa@gmail.com>
2024-07-16 17:00:20 -0300

    use relative numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.relativenumber = false -- Relative line numbers
 vim.g.autochdir = true
 vim.opt.scrolloff = 10
 vim.opt.title = true
