4a88296324e91c73c7d6acd5146acdda13ed347a
emigpa <emigpa@gmail.com>
2024-11-18 20:07:53 -0300

    new dashboard

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        sections = {
+          { section = "header" },
+          { section = "keys", gap = 1, padding = 1 },
+          { section = "startup" },
+          {
+            section = "terminal",
+            cmd = "pokemon-colorscripts -r; sleep .1",
+            random = 10,
+            pane = 2,
+            indent = 4,
+            height = 30,
+          },
+        },
+      },
+    },
+  },
+}
