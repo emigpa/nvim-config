851bded11b7c498d0bc9ac1332115be4e0c4ebc7
emigpa <emigpa@gmail.com>
2024-12-04 11:47:34 -0300

    added yanky

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- { import = "lazyvim.plugins.extras.coding.mini-surround" },
     -- { import = "lazyvim.plugins.extras.editor.mini-files" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.editor.dial" },
     { import = "lazyvim.plugins.extras.editor.mini-diff" },
     { import = "lazyvim.plugins.extras.editor.mini-move" },
