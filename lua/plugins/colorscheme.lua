71c41c1187bd19de54db80a46dfaa3bb691dde13
emigpa <emigpa@gmail.com>
2025-01-30 16:32:12 -0300

    set colorscheme to rosepine darker

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,38 +4,6 @@ return {
     {
       "LazyVim/LazyVim",
       opts = {
-        dark_variant = "moon",
-        styles = { italic = false },
-        palette = {
-          dawn = {
-            no_bg = "#faf4ed",
-          },
-          moon = {
-            gold = "#f6d5a7",
-            foam = "#a1d1da",
-            iris = "#d9c7ef",
-            rose = "#ebbcba",
-            pine = "#437e91",
-            no_bg = "#000000",
-          },
-        },
-        highlight_groups = {
-          Normal = { bg = "no_bg" },
-          Directory = { fg = "foam", bold = false },
-          StatusLine = { bg = "surface", fg = "subtle" },
-          StatusLineTerm = { link = "StatusLine" },
-          StatusLineNC = { link = "StatusLine" },
-          --- gitsigns
-          StatusLineGitSignsAdd = { bg = "surface", fg = "pine" },
-          StatusLineGitSignsChange = { bg = "surface", fg = "gold" },
-          StatusLineGitSignsDelete = { bg = "surface", fg = "rose" },
-          --- diagnostics
-          StatusLineDiagnosticSignError = { bg = "surface", fg = "love" },
-          StatusLineDiagnosticSignWarn = { bg = "surface", fg = "gold" },
-          StatusLineDiagnosticSignInfo = { bg = "surface", fg = "foam" },
-          StatusLineDiagnosticSignHint = { bg = "surface", fg = "iris" },
-          StatusLineDiagnosticSignOk = { bg = "surface", fg = "pine" },
-        },
         colorscheme = "rose-pine",
       },
     },
@@ -46,5 +14,42 @@ return {
     priority = 1000,
     opts = {},
   },
-  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      dark_variant = "moon",
+      styles = { italic = false },
+      palette = {
+        dawn = {
+          no_bg = "#faf4ed",
+        },
+        moon = {
+          gold = "#f6d5a7",
+          foam = "#a1d1da",
+          iris = "#d9c7ef",
+          rose = "#ebbcba",
+          pine = "#437e91",
+          no_bg = "#000000",
+        },
+      },
+      highlight_groups = {
+        Normal = { bg = "no_bg" },
+        Directory = { fg = "foam", bold = false },
+        StatusLine = { bg = "surface", fg = "subtle" },
+        StatusLineTerm = { link = "StatusLine" },
+        StatusLineNC = { link = "StatusLine" },
+        --- gitsigns
+        StatusLineGitSignsAdd = { bg = "surface", fg = "pine" },
+        StatusLineGitSignsChange = { bg = "surface", fg = "gold" },
+        StatusLineGitSignsDelete = { bg = "surface", fg = "rose" },
+        --- diagnostics
+        StatusLineDiagnosticSignError = { bg = "surface", fg = "love" },
+        StatusLineDiagnosticSignWarn = { bg = "surface", fg = "gold" },
+        StatusLineDiagnosticSignInfo = { bg = "surface", fg = "foam" },
+        StatusLineDiagnosticSignHint = { bg = "surface", fg = "iris" },
+        StatusLineDiagnosticSignOk = { bg = "surface", fg = "pine" },
+      },
+    },
+  },
 }
