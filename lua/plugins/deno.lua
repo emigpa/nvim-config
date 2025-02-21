c03a2587ae2124766885b35b92d0edda64774424
emigpa <emigpa@gmail.com>
2025-02-21 18:33:51 -0300

    add deno support and disable vtls when deno

---
+++ lua/plugins/deno.lua
@@ -0,0 +1,28 @@
+local nvim_lsp = require("lspconfig")
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        denols = {
+          filetypes = { "typescript", "typescriptreact", "javascript" },
+          root_dir = function(...)
+            return nvim_lsp.util.root_pattern("deno.jsonc", "deno.json")(...)
+          end,
+        },
+        vtsls = {
+          root_dir = function()
+            return not vim.fs.root(0, { "deno.json", "deno.jsonc" })
+              and vim.fs.root(0, {
+                "tsconfig.json",
+                "jsconfig.json",
+                "package.json",
+                ".git",
+              })
+          end,
+        },
+      },
+    },
+  },
+}
