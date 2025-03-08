63ac58d31ca16eb19f06303acefed348f8d90c88
emigpa <emigpa@gmail.com>
2025-03-08 18:02:39 -0300

    disable vtsls for eslint projects

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,12 +8,43 @@ return {
       setup = {
         volar = function()
           require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "volar" then
+            -- if client.name == "volar" then
+            --   client.server_capabilities.documentFormattingProvider = false
+            -- end
+            local function checkIfESLintExists()
+              -- List of possible ESLint config files
+              local eslintConfigFiles =
+                { ".eslintrc.js", ".eslintrc.json", ".eslintrc.yml", ".eslintrc.yaml", "eslint.json" }
+
+              -- Check if any of these files exist in the current directory
+              for _, configFile in ipairs(eslintConfigFiles) do
+                local file = io.open(configFile, "r") -- Try to open the file in read mode
+                if file then
+                  io.close(file) -- Close the file if it exists
+                  return true -- ESLint config file found
+                end
+              end
+
+              return false -- No ESLint config file found
+            end
+            -- if project uses eslint disable vtsls formatting
+            if client.name == "vtsls" and checkIfESLintExists() then
               client.server_capabilities.documentFormattingProvider = false
             end
           end)
         end,
       },
+      -- servers = {
+      --   vtsls = {
+      --     enabled = function()
+      --       local eslint_config = vim.fs.find(
+      --         { ".eslintrc", ".eslintrc.json", ".eslintrc.js", ".eslintrc.yaml", ".eslintrc.yml" },
+      --         { upward = true, path = vim.fn.getcwd() }
+      --       )
+      --       return #eslint_config == 0
+      --     end,
+      --   },
+      -- },
     },
   },
   -- {
