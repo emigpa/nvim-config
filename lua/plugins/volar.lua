ad037e776ea9dddaaaaab33a97caf254a15dedc0
emigpa <emigpa@gmail.com>
2025-07-03 14:04:59 -0300

    fix volar version

--- lua/plugins/volar.lua
+++ lua/plugins/volar.lua
@@ -1,16 +1,20 @@
 return {
-  --   {
-  --     "neovim/nvim-lspconfig",
-  --     opts = {
-  --       servers = {
-  --         volar = {
-  --           autostart = true,
-  --           filetypes = {
-  --             "typescript",
-  --             "vue",
-  --           },
-  --         },
-  --       },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = {
+  --     -- Mason core settings (optional)
+  --     ensure_installed = {
+  --       "vue_ls",
   --     },
   --   },
+  -- },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "volar@2.2.10",
+      },
+      automatic_installation = true, -- Automatically install listed servers
+    },
+  },
 }
