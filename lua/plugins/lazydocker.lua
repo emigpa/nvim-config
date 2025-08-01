77aaae002b6f03330b77db941ab8ceb5d2d88d94
emigpa <emigpa@gmail.com>
2025-08-01 12:58:00 -0300

    removed lazydocker plugin, add a floating term setup

--- lua/plugins/lazydocker.lua
+++ lua/plugins/lazydocker.lua
@@ -1,8 +1,34 @@
 return {
-  "crnvl96/lazydocker.nvim",
-  event = "VeryLazy",
-  opts = {}, -- automatically calls `require("lazydocker").setup()`
-  dependencies = {
-    "MunifTanjim/nui.nvim",
+  {
+    "akinsho/toggleterm.nvim",
+    opts = {
+      direction = "float",
+      float_opts = {
+        border = "double",
+        width = function()
+          return math.floor(vim.o.columns * 0.9) -- 80% width
+        end,
+        height = function()
+          return math.floor((vim.o.lines - 2) * 0.9) -- 80% height
+        end,
+        row = function()
+          return math.floor(((vim.o.lines - 2) - ((vim.o.lines - 2) * 0.9)) / 2)
+        end,
+        col = function()
+          return math.floor((vim.o.columns - (vim.o.columns * 0.9)) / 2)
+        end,
+      },
+    },
+    keys = {
+      {
+        "<leader>ld",
+        function()
+          local Terminal = require("toggleterm.terminal").Terminal
+          local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true, direction = "float" })
+          lazydocker:toggle()
+        end,
+        desc = "Toggle LazyDocker",
+      },
+    },
   },
 }
