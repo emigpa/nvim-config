c7c48519eafe841a03d07cb8ae4cae199f1b9ddd
emigpa <emigpa@gmail.com>
2025-08-02 18:46:39 -0300

    extra yaml

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -40,6 +40,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
     -- import/override with your plugins
     { import = "plugins" },
   },
