30204bf44486cc7b62270c550380f8fab744f634
emigpa <emigpa@gmail.com>
2026-01-29 12:23:01 -0300

    dont keep buffers open by default, mark them with bk

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+---@diagnostic disable: undefined-global vim
+
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  desc = "Make file buffers disposable by default",
+  callback = function()
+    if vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
+      vim.bo.bufhidden = "wipe"
+    end
+  end,
+})
